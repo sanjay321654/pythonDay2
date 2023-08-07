3bb58ea2a8f6c93039d1b0f20e3f6a18d9180431
Sanjay M R <sanjayilaya2002@gmail.com>
2023-08-07 19:35:41 +0530

    uploading my day2 exercise3

---
+++ Lifeinweeks.py
@@ -0,0 +1,14 @@
+
+age = input("What is your current age? ")
+
+
+#Write your code below this line 👇
+new_age = int(age)
+years_remaining = 90 - new_age
+days_remaining = years_remaining*365
+weeks_remaining = years_remaining*52
+months_remaining = years_remaining*12
+print(f"you have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left." )
+
+
+
