ee866c079c337d8eadfeba29e4e01594a4f18091
Sanjay M R <sanjayilaya2002@gmail.com>
2023-08-07 20:07:53 +0530

    Update BMICalc.py

--- BMICalc.py
+++ BMICalc.py
@@ -1,13 +1,7 @@
-height = float(input("what your height? "))
-weight = float(input("what is your weight?"))
-#bmi formula is weight divided by height square
-bmi = round(weight / height**2)
-# line alignments are very importany in if else elif conditions.
-if bmi <18.5:
-  print(f"your bmi is {bmi}, you are underweight. ")
-elif bmi<25:
-  print(f"your bmi is {bmi}, you are normal weight.")
-elif bmi<35:
-  print(f"your bmi is {bmi}, your are obese. ")
-else:
-  print(f"your bmi is {bmi}, your clinically obese.")
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+
+bmi = int(weight) /float(height)**2
+
+print(bmi)
